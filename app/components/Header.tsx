a72d398c1480730f82f8bb772d2eb3b3cf9bc4f6
avtr-inanca <ian.nazareno@avattar.com>
2025-12-01 10:46:51 -0300

    UI scaling fixed for mobile devices

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
 	};
 
 	return (
-		<header className="bg-white dark:bg-[var(--muted)] shadow-md mb-6 sticky top-0 z-50">
+		<header className="bg-white dark:bg-[var(--muted)] shadow-md sticky top-0 z-50">
 			<nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex flex-col sm:flex-row justify-between items-center gap-4">
 				<Link href="/home" className="text-4xl font-bold text-[var(--primary)]">
 					My Anime Log
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
 	return (
 		<html>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
 				<AuthProvider>
 					<LanguageProvider>
 						<Header/>
